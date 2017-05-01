6836b813e50caad8a878d6b258857e2508d5011c
djechlin <dan@unroll.me>
2017-04-30 22:36:38 -0400

    write some network formations

---
+++ network_formation_game.py
@@ -0,0 +1,32 @@
+'''Module for playing the network centrality game with greedy strategies.'''
+
+import itertools
+import networkx as nx
+
+from best_actions import *
+
+def play_one_player_on_graph(graph, player):
+    for i in xrange(20):
+        best = best_addition(graph, player)
+        print(best)
+        if best['improvement'] >= 0:
+            graph.add_edge(*best['edge'])
+        else:
+            print('No more improvements')
+            return
+
+def play_one_player_on_empty_graph():
+    graph = nx.empty_graph(20)
+    play_one_player_on_graph(graph, 0)
+
+
+def play_one_player_on_ring():
+    graph = nx.empty_graph(20);
+    for i in xrange(19):
+        graph.add_edge(i, i+1)
+    graph.add_edge(19, 0)
+    play_one_player_on_graph(graph, 0)
+
+
+
+play_one_player_on_ring()
